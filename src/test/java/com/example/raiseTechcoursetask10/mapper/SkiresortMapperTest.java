2bd0b45a9d8a9ec66a4634f6ef369d05b6902362
yochan0120 <yoko_whiterosa.alba@gamma.ocn.ne.jp>
2023-07-31 20:21:45 +0900

    Mapperの単体テストエラー

--- src/test/java/com/example/raiseTechcoursetask10/mapper/SkiresortMapperTest.java
+++ src/test/java/com/example/raiseTechcoursetask10/mapper/SkiresortMapperTest.java
@@ -31,13 +31,16 @@ class SkiresortMapperTest {
     void 全てのスキーリゾートが取得できること() {
         List<Skiresort> skiresorts = skiresortMapper.findAll();
         assertThat(skiresorts)
-                .hasSize(5)
+                .hasSize(8)
                 .contains(
                         new Skiresort(1, "rusutsu", "hokkaido", "雪質が良くて、カービング練習に最適。"),
                         new Skiresort(2, "zao", "yamagata", "凄く広くて、樹氷が綺麗だった。"),
                         new Skiresort(3, "marunuma-kogen", "gunma", "初心者から上級者まで楽しめる。ゲレ食のアクセスが悪い"),
                         new Skiresort(4, "hakubaGoryu", "nagano", "GWまで滑れる。外国人が多くていつも混んでる。"),
-                        new Skiresort(5, "nozawa-onsen", "nagano", "ゴンドラが10人乗りでガラス張りに変わった。外国人ばかりで激混み")
+                        new Skiresort(5, "nozawa-onsen", "nagano", "ゴンドラが10人乗りでガラス張りに変わった。外国人ばかりで激混み"),
+                        new Skiresort(6, "安比高原", "岩手県", "いつも天気が悪い。"),
+                        new Skiresort(7, "月山", "山形県", "5月から雪がなくなるまで営業してて、全面コブになる。"),
+                        new Skiresort(8, "イエティ", "静岡県", "10月オープンで日本一早い。激混み")
                 );
     }
 }
