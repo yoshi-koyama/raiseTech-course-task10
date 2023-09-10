c1fb0337a9be3ddb0904e0ad4a72aa83080b4366
yochan0120 <yoko_whiterosa.alba@gamma.ocn.ne.jp>
2023-09-10 17:09:46 +0900

    Service単体テスト：全てのデータを取得できることの実装

--- src/test/java/com/example/raiseTechcoursetask10/service/SkiresortServiceImplTest.java
+++ src/test/java/com/example/raiseTechcoursetask10/service/SkiresortServiceImplTest.java
@@ -9,6 +9,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -34,5 +35,24 @@ class SkiresortServiceImplTest {
         Skiresort actual = skiresortServiceImpl.findById(1);
         assertThat(actual).isEqualTo(new Skiresort(1, "たかつえ", "福島県", "いつも空いてて1枚バーンが気持ちいい"));
         verify(skiresortMapper, times(1)).findById(1);
+    }
+
+    @Test
+    public void 全てのスキー場情報を取得できること() {
+
+        List<Skiresort> skiresorts = List.of(
+                new Skiresort(1, "Cadrona", "NZ", "パイプのnationals公式大会で優勝して、副賞モルディブ1週間旅行だった！ゲレンデはコンクリートみたいに硬い"),
+                new Skiresort(2, "Whistler", "canada", "滞在2週間の半分以上雨で、記録的な少雪な年だった"),
+                new Skiresort(3, "Mt.Hood", "Oregon", "標高が高すぎて高山病になった。ガスってる日に2000m以上続く急斜面で滑落した"));
+
+        // 依存しているSkiresortMapperをモック化する
+        // skiresortMapperのfindAll()メソッドが呼ばれた時に、skiresortsリストを返す
+        doReturn(skiresorts).when(skiresortMapper).findAll();
+
+        // test実行
+        List<Skiresort> actual = skiresortServiceImpl.findAll();
+        // actual(実際)の値がモック化した値(skiresorts)と等しいか検証する
+        assertThat(actual).isEqualTo(skiresorts);
+        verify(skiresortMapper, times(1)).findAll();
     }
 }
