7dcb5f8fb3f0c7ce4cd0e6bc19e75323ebb19d67
yochan0120 <yoko_whiterosa.alba@gamma.ocn.ne.jp>
2023-10-18 20:15:22 +0900

    it:新規スキーリゾートを登録するテストの実装/it-memo:追記

--- src/test/java/com/example/raisetechcoursetask10/integrationtest/SkiresortRestApiIntegrationTest.java
+++ src/test/java/com/example/raisetechcoursetask10/integrationtest/SkiresortRestApiIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.example.raisetechcoursetask10.integrationtest;
 
 import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
 import com.github.database.rider.spring.api.DBRider;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -10,12 +11,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.nio.charset.StandardCharsets;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -33,7 +37,7 @@ public class SkiresortRestApiIntegrationTest {
         @Transactional
         void スキーリゾートを全件取得したときステータスコードが200を返すこと() throws Exception {
             String response = mockMvc.perform(MockMvcRequestBuilders.get("/skiresorts"))
-                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(status().isOk())
                     .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
 
             JSONAssert.assertEquals("""
@@ -63,7 +67,7 @@ public class SkiresortRestApiIntegrationTest {
         @Transactional
         void 存在するIDのスキーリゾートを取得した時ステータスコードが200を返すこと() throws Exception {
             String response = mockMvc.perform(MockMvcRequestBuilders.get("/skiresorts/{id}", 3))
-                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(status().isOk())
                     .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
 
             JSONAssert.assertEquals("""
@@ -79,7 +83,29 @@ public class SkiresortRestApiIntegrationTest {
         @Transactional
         void 存在しないIDのスキーリゾートを取得した時ステータスコードが404エラーを返すこと() throws Exception {
             mockMvc.perform(MockMvcRequestBuilders.get("/skiresorts/{id}", 99))
-                    .andExpect(MockMvcResultMatchers.status().isNotFound());
+                    .andExpect(status().isNotFound());
+        }
+    }
+
+    @Nested
+    class CreateTest {
+        @Test
+        @DataSet(value = "datasets/it/skiresort.yml")
+        @ExpectedDataSet(value = "datasets/it/create-skiresort.yml", ignoreCols = {"id", "customerEvaluation"})
+        @Transactional
+        void 新規のスキーリゾートを登録した時ステータスコードが201を返すこと() throws Exception {
+            String response = mockMvc.perform(MockMvcRequestBuilders.post("/skiresorts")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content("""
+                                    {
+                                        "id": 4,
+                                        "name": "Whistler",
+                                        "area": "Canada",
+                                        "customerEvaluation": "Obtained Canadian snowboard instructor license"
+                                    }
+                                    """))
+                    .andExpect(MockMvcResultMatchers.status().isCreated())
+                    .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
         }
     }
 }
